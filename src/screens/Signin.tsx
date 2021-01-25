f84233d1ed8aaae3c5ea5d4851ca7c3f143b23a5
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-01-25 15:50:56 -0300

    Updates sign in screen

--- src/screens/Signin.tsx
+++ src/screens/Signin.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
-import {View, Text, Image, Dimensions} from 'react-native';
+import {View, Text, Image, TouchableOpacity, Dimensions} from 'react-native';
 import EStyleSheet from 'react-native-extended-stylesheet';
 import Button from '../components/Button';
+import Input from '../components/Input';
 import SignScaffold from '../components/SignScaffold';
 
+const {width: widthScreen, height: heightScreen} = Dimensions.get('screen');
 const logo = require('../../assets/images/logo-colour.png');
 
 const Signin = () => {
@@ -11,16 +13,28 @@ const Signin = () => {
     <>
       <SignScaffold>
         <Image style={styles.logo} source={logo} />
-        <View style={styles.headerText}>
-          <Text>Logging</Text>
-          <Text>Enter your emails and password</Text>
-        </View>
         <View style={styles.form}>
+          <View>
+            <Text style={styles.headerTitle}>Loging</Text>
+            <Text style={styles.headerSubtitle}>
+              Enter your emails and password
+            </Text>
+          </View>
+          <Input label="Email" />
+          <View style={{marginTop: heightScreen * 0.011}} />
+          <Input label="Password" />
+          <TouchableOpacity style={styles.forgotButtonBox}>
+            <Text style={styles.infoText}>Forgot your password?</Text>
+          </TouchableOpacity>
           <Button onPress={() => null} text="Log in" />
-        </View>
-        <View style={styles.footerText}>
-          <Text>Don’t have an account?</Text>
-          <Text>Singup</Text>
+          <View style={styles.footer}>
+            <Text style={styles.infoText}>Don’t have an account?</Text>
+            <TouchableOpacity>
+              <Text style={[styles.infoText, styles.greenInfoText]}>
+                Singup
+              </Text>
+            </TouchableOpacity>
+          </View>
         </View>
       </SignScaffold>
     </>
@@ -30,6 +44,47 @@ const Signin = () => {
 const styles = EStyleSheet.create({
   logo: {
     alignSelf: 'center',
+    marginTop: heightScreen * 0.032,
+    marginBottom: heightScreen * 0.112,
+  },
+  form: {
+    paddingHorizontal: widthScreen * 0.06,
+  },
+  headerTitle: {
+    fontSize: '1.625rem',
+    lineHeight: '1.625rem',
+    height: '1.625rem',
+    fontFamily: 'Mulish-SemiBold',
+    marginBottom: heightScreen * 0.017,
+    color: '$blackColour',
+  },
+  headerSubtitle: {
+    fontSize: '1rem',
+    lineHeight: '1rem',
+    height: '1rem',
+    fontFamily: 'Mulish-Regular',
+    marginBottom: heightScreen * 0.045,
+    color: '#7C7C7C',
+  },
+  forgotButtonBox: {
+    alignItems: 'flex-end',
+    marginBottom: heightScreen * 0.033,
+  },
+  infoText: {
+    fontFamily: 'Mulish-Medium',
+    fontSize: '0.875rem',
+    color: '$blackColour',
+    letterSpacing: '0.05rem',
+  },
+  footer: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginTop: heightScreen * 0.028,
+  },
+  greenInfoText: {
+    color: '$greenColour',
+    marginLeft: 5.0,
   },
 });
 
