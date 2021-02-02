173d402cc8440e8199491738b599c3e5dcc94c19
feMoraes0 <fernandomoraes.lopes@gmail.com>
2021-02-01 22:59:14 -0300

    Adds new some asset images

---
+++ src/helpers/Data.tsx
@@ -0,0 +1,75 @@
+const productCategories = [
+  {
+    name: 'Frash Fruits & Vegetable',
+    image: '../../assets/images/product_categories/fruits.png',
+    bgColour: 'rgba(83, 177, 117, 0.1)',
+  },
+  {
+    name: 'Cooking Oil & Ghee',
+    image: '../../assets/images/product_categories/oil.png',
+    bgColour: 'rgba(248, 164, 76, 0.1)',
+  },
+  {
+    name: 'Meat & Fish',
+    image: '../../assets/images/product_categories/meat.png',
+    bgColour: 'rgba(247, 265, 147, 0.25)',
+  },
+  {
+    name: 'Bakery & Snacks',
+    image: '../../assets/images/product_categories/bakery.png',
+    bgColour: 'rgba(211, 176, 224, 0.25)',
+  },
+  {
+    name: 'Dairy & Eggs',
+    image: '../../assets/images/product_categories/dairy.png',
+    bgColour: 'rgba(253, 229, 152, 0.25)',
+  },
+  {
+    name: 'Beverages',
+    image: '../../assets/images/product_categories/beverages.png',
+    bgColour: 'rgba(183, 223, 245, 0.25)',
+  },
+];
+
+const products = {
+  beverages: [
+    {
+      name: 'Diet Coke',
+      description: '355ml, Price',
+      price: '1.99',
+      image: '../../assets/images/products/beverages/diet_coke.png',
+    },
+    {
+      name: 'Sprite Can',
+      description: '325ml, Price',
+      price: '1.50',
+      image: '../../assets/images/products/beverages/sprite.png',
+    },
+    {
+      name: 'Apple & Grape Juice',
+      description: '355ml, Price',
+      price: '15.99',
+      image: '../../assets/images/products/beverages/apple_juice.png',
+    },
+    {
+      name: 'Orange Juice',
+      description: '355ml, Price',
+      price: '15.99',
+      image: '../../assets/images/products/beverages/orange_juice.png',
+    },
+    {
+      name: 'Coca Cola Can',
+      description: '355ml, Price',
+      price: '4.99',
+      image: '../../assets/images/products/beverages/coke.png',
+    },
+    {
+      name: 'Pepsi Can',
+      description: '325ml, Price',
+      price: '4.99',
+      image: '../../assets/images/products/beverages/pepsi.png',
+    },
+  ],
+};
+
+export {productCategories, products};
